646d35b9f81812499904fb2614ea3e91f50387f3
Avraham Hamu 🏂🏼 <gibor7@gmail.com>
2025-11-13 17:38:43 +0200

    mono-repo: copying rn app

---
+++ apps/react-native-app/src/services/http.service.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { ALL, API_KEY, TMDB_BASE_URL } from '@/common/constants';
+import { Movie } from '@/types/common.types';
+
+export function getUrl(page: number, year?: string, genreId?: number): string {
+  let url = `${TMDB_BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false`;
+
+  if (year && year !== ALL) {
+    const yearStart = parseInt(year);
+    const yearEnd = yearStart;
+    url += `&primary_release_date.gte=${yearStart}-01-01&primary_release_date.lte=${yearEnd}-12-31`;
+  }
+  if (genreId) {
+    url += `&with_genres=${genreId}`;
+  }
+
+  url += `&page=${page}`;
+  console.log(`====> DEBUG url: `, url, 'page:', page, 'year:', year, 'genreId:', genreId);
+  return url;
+}
+
+export const movieService = {
+  async fetchMovies(url: string): Promise<Movie[]> {
+    const response = await axios.get(url);
+    return response.data.results;
+  },
+};
---
+++ apps/react-native-app/src/services/utils.ts
@@ -0,0 +1,18 @@
+import { GENRE_MAP_INVERTED } from '@/common/constants';
+import { Movie } from '@/types/common.types';
+
+export const getGenreNames = (genreIds: number[]): string => {
+  if (!genreIds || genreIds.length === 0) {
+    return 'Unknown';
+  }
+
+  return genreIds.map(id => GENRE_MAP_INVERTED[id] || 'Unknown').join(', ');
+};
+
+export const getMoviesByRating = (moviesList: Movie[]): Movie[] => {
+  return moviesList.sort((a, b) => b.vote_average - a.vote_average);
+};
+
+export function getPosterUrl(posterPath: string): string {
+  return posterPath ? `https://image.tmdb.org/t/p/w500${posterPath}` : 'https://via.placeholder.com/500x750?text=No+Poster';
+}
