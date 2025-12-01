bed130c5e2051d24ab0eaa166ac457cb899a1361
Avraham Hamu 🏂🏼 <gibor7@gmail.com>
2025-12-01 19:26:49 +0200

    use common constants

--- apps/lynxjs-app/src/common/LX_constants.ts
+++ apps/lynxjs-app/src/common/LX_constants.ts
@@ -1,6 +1,11 @@
+import { ALL, GENRES_FILTER, GENRE_MAP, GENRE_MAP_INVERTED, TMDB_BASE_URL, YEARS_FILTER } from '@repo/constants';
+
 export const API_KEY = process.env.TMDB_API_KEY;
 
 export const IMDB_CHUNK_SIZE = 20;
 
 export const IS_ANDROID = SystemInfo.platform === 'Android';
 export const IS_IOS = SystemInfo.platform === 'iOS';
+
+// Re-export common constants
+export { GENRE_MAP, GENRE_MAP_INVERTED, TMDB_BASE_URL, YEARS_FILTER, GENRES_FILTER, ALL };
--- apps/react-native-app/src/common/constants.ts
+++ apps/react-native-app/src/common/constants.ts
@@ -1,6 +1,24 @@
 import { Platform } from 'react-native';
+import {
+  GENRE_MAP,
+  GENRE_MAP_INVERTED,
+  TMDB_BASE_URL,
+  YEARS_FILTER,
+  GENRES_FILTER,
+  ALL
+} from '@repo/constants';
 
 export const API_KEY = process.env.EXPO_PUBLIC_TMDB_API_KEY;
 
 export const IS_ANDROID = Platform.OS === 'android';
 export const IS_IOS = Platform.OS === 'ios';
+
+// Re-export common constants
+export {
+  GENRE_MAP,
+  GENRE_MAP_INVERTED,
+  TMDB_BASE_URL,
+  YEARS_FILTER,
+  GENRES_FILTER,
+  ALL
+};
