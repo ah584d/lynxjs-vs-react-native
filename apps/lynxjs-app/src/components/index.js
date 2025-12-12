701379bae738582ac48665679adab3762256832f
Avraham Hamu 🏂🏼 <gibor7@gmail.com>
2025-12-12 11:20:50 +0200

    delete *.js

--- apps/lynxjs-app/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import NavBar from './NavBar/LX_NavBar.jsx';
-import PageView from './PageView/LX_PageView.jsx';
-import SafeViewArea from './SafeViewArea/LX_SafeViewArea.jsx';
-export { SafeViewArea, PageView, NavBar };
