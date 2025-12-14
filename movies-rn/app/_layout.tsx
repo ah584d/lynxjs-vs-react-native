512377ae2f28b51cda64fe3998ff747d010a4588
Avraham Hamu 🏂🏼 <gibor7@gmail.com>
2025-12-14 19:41:05 +0200

    final disconnect from workspace

--- movies-rn/app/_layout.tsx
+++ movies-rn/app/_layout.tsx
@@ -1,24 +1,27 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { RootNavigator } from '@/components/RootNavigator';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
+export default function App() {
   const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
 
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <RootNavigator />
+      </ThemeProvider>
+      <StatusBar style='auto' />
+    </SafeAreaProvider>
   );
 }
