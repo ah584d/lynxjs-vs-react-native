7af31ccaab9933c5fa3bbad98d73d594aaa98588
Avraham Hamu 🏂🏼 <gibor7@gmail.com>
2026-04-09 01:45:42 +0300

    lx: add curtain animation

--- apps/lynxjs-app/src/components/MenuCurtain/LX_MenuCurtain.tsx
+++ apps/lynxjs-app/src/components/MenuCurtain/LX_MenuCurtain.tsx
@@ -1,5 +1,6 @@
 import { type ReactElement } from '@lynx-js/react';
 import { useMovieStore } from '@fennex-sand/hooks';
+import classNames from 'classnames';
 import { Hamburger } from '../Hamburger/LX_Hamburger';
 import { Performance } from '../PerformanceMetrics/LX_PerformanceMetrics';
 import styles from './menuCurtain.module.scss';
@@ -8,15 +9,15 @@ export function MenuCurtain(): ReactElement {
   const menuOpened = useMovieStore(state => state.menuOpened);
 
   return (
-    <>
-      {menuOpened && (
-        <view className={styles.container}>
-          <view className={styles.hamburger}>
-            <Hamburger />
-          </view>
-          <Performance />
-        </view>
-      )}
-    </>
+    <view
+      className={classNames(styles.container, {
+        [styles.open]: menuOpened,
+        [styles.closed]: !menuOpened,
+      })}>
+      <view className={styles.hamburger}>
+        <Hamburger />
+      </view>
+      <Performance />
+    </view>
   );
 }
